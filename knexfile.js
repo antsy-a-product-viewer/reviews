95babef1f021de951b1f84f8195a9ec0a688755a
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-07 12:01:59 -0800

    Add newline to eslint file

--- knexfile.js
+++ knexfile.js
@@ -10,30 +10,6 @@ module.exports = {
       user:     mysqlConfig.mysqlUsername,
       password: mysqlConfig.mysqlPassword
     },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
     pool: {
       min: 2,
       max: 10
@@ -42,5 +18,4 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-
 };
