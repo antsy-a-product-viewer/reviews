0e638710a35175533dedfc8550e50da84c2e7c4f
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-07 19:49:22 -0800

    Complete migration file for items

--- migrations/20190207193810_items.js
+++ migrations/20190207193810_items.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('items', table => {
+    table.increments('id')
+    table.string('name')
+    table.float('price')
+    table.number('store_id')
+    table.string('img_url')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('items')
 };
