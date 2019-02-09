f4109991ba87ecf07fa38a7ad1ba2ada5ee54f4f
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-08 17:30:20 -0800

    Add 400 status for error in GET request

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ app.get('/items/:itemId/reviews', (req, res) => {
   db.getAllReviewsForItem(1, (err, data) => {
     if (err) {
       console.log('error getting item reviews: ', err);
+      res.status(400).send(err);
     } else {
       var parsedData = JSON.parse(data)
       console.log('success getting reviews for item');
