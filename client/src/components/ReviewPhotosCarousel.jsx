6614a23dd618cc1c73585e5b9b223fcb09a6342f
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-12 13:11:42 -0800

    Create css file for reviewPhotosCarousel

--- client/src/components/ReviewPhotosCarousel.jsx
+++ client/src/components/ReviewPhotosCarousel.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-
-const reviewImageStyle = {
-  clear: "both",
-  maxWidth: 100,
-  maxHeight: 100,
-  margin: 10
-}
+import styles from './css/reviewPhotosCarousel.css.js';
 
 const ReviewPhotosCarousel = (props) => {
   return (
-    <div className="review-photos">
-      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={reviewImageStyle}></img>
-      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={reviewImageStyle}></img>
-      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={reviewImageStyle}></img>
-      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={reviewImageStyle}></img>
-      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={reviewImageStyle}></img>
+    <div>
+      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={styles.reviewImage}></img>
+      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={styles.reviewImage}></img>
+      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={styles.reviewImage}></img>
+      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={styles.reviewImage}></img>
+      <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_300x300.1735567682_9rntz5h8.jpg?version=0" style={styles.reviewImage}></img>
     </div>
-  )
-}
+  );
+};
 
 export default ReviewPhotosCarousel;
