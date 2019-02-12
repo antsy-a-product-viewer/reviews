620512f2295f338cf0038e320838a58de07be1e6
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-12 10:21:16 -0800

    Add basic styling to modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
-const Modal = (props) => {
+const modalStyle = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  background: "rgba(0, 0, 0, 0.6)"
+}
 
+const modalMainStyle = {
+  position: "fixed",
+  background: "white",
+  width: "80%",
+  height: "auto",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%,-50%)"
+}
+
+const Modal = (props) => {
   if (!props.showModal) {
     return null;
   } else {
     return (
-      <div>
-        <h1>{props.children}</h1>
-        <div>
+      <div className="modal" style={modalStyle}>
+        <section className="modal-main" style={modalMainStyle}>
+          {props.children}
           <button onClick={props.onClose}>Close</button>
-        </div>
+        </section>
       </div>
     )
   }
