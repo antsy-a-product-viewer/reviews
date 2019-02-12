748781c446d7c0927b2c018ecdfa97bbdc28205a
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-12 12:57:24 -0800

    Create css file for modalStyles

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
-
-const modalStyle = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  background: "rgba(0, 0, 0, 0.6)"
-}
-
-const modalMainStyle = {
-  position: "fixed",
-  background: "white",
-  width: "80%",
-  height: "auto",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%,-50%)"
-}
+import styles from './css/modalStyles.css.js'
 
 const Modal = (props) => {
   if (!props.showModal) {
     return null;
   } else {
     return (
-      <div className="modal" style={modalStyle}>
-        <section className="modal-main" style={modalMainStyle}>
+      <div style={styles.modal}>
+        <section style={styles.modalMain}>
           <img src="https://i.etsystatic.com/iap/c08960/1735567682/iap_640x640.1735567682_9rntz5h8.jpg?version=0"></img>
           <button onClick={props.onClose}>Close</button>
         </section>
       </div>
-    )
+    );
   }
-}
+};
 
 export default Modal;
