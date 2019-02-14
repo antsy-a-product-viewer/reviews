6a5b57c927c0feb8128cf93fcd17ef8f1fb4ef7b
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-14 14:32:52 -0800

    Add snapshot test to modal

--- client/src/__tests__/modal.test.js
+++ client/src/__tests__/modal.test.js
@@ -2,4 +2,15 @@ import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
-import Container from '../components/Container.jsx';
+import Modal from '../components/Modal.jsx';
+
+configure({ adapter: new Adapter() });
+
+it('should render correctly when props.showModal is true', () => {
+  const props = {
+    showModal: true
+  }
+  const modal = shallow(<Modal showModal={props.showModal}/>);
+  
+  expect(modal).toMatchSnapshot();
+});
