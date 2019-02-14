2b1efcdd465788902dfcdf4b2224a54369df944a
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-14 13:41:00 -0800

    Add test files for all components

---
+++ client/src/__tests__/item.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+import Container from '../components/Container.jsx';
