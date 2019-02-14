6147faaaf62381148c2da0ab221434d3313e6904
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-14 08:05:41 -0800

    Remove commented code from modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -10,15 +10,11 @@ const Modal = (props) => {
     return (
       <div style={styles.modal} onClick={props.onClose}>
         <section style={styles.modalMain}>
-          {/* <div style={styles.reviewContent}> */}
           <img style={styles.image} src="https://i.etsystatic.com/iap/c08960/1735567682/iap_640x640.1735567682_9rntz5h8.jpg?version=0"></img>
           <ReviewContainer showPrice="true"/>
-          {/* </div> */}
-          {/* <div> */}
           <button style={styles.button} onClick={props.onClose}>
             <img style={styles.buttonImg}src="https://cdn0.iconfinder.com/data/icons/elasto-online-store/26/00-ELASTOFONT-STORE-READY_close-512.png"></img>
           </button>
-          {/* </div> */}
         </section>
       </div>
     );
